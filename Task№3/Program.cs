80deb10d68bc6b74096000cd3ad27621251c311e
Vladislav <zomba4o4ek100@gmail.com>
2022-12-17 20:34:46 +0300

    Third task complete

---
+++ Task№3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.WriteLine("Enter number ");
+int num = Convert.ToInt32(Console.ReadLine());
+        if ((num % 2) == 0)
+        {
+            Console.WriteLine(true);
+        }
+        else
+        {
+            Console.WriteLine(false);
+        }
