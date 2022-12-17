3f3c6313a68d0470990638ff3df87672294d7ec7
Vladislav <zomba4o4ek100@gmail.com>
2022-12-17 20:07:04 +0300

    Second task complete

---
+++ Task№2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine("First number ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Second number ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Third number ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
